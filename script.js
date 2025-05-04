7e111e5cf9f5596103d8861fe9b1078df89e3f52
Dr. Dracula <141348584+dankrusef@users.noreply.github.com>
2025-05-04 12:34:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// Optional spooky sounds (disabled by default)
+
+document.addEventListener('DOMContentLoaded', function() {
+  // Uncomment below to enable background spooky sound:
+  /*
+  let audio = new Audio('https://www.fesliyanstudios.com/play-mp3/387');
+  audio.loop = true;
+  audio.volume = 0.2;
+  audio.play();
+  */
+});
